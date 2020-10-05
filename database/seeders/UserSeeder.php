2166dbb3d9f85c5624da82abe4da322f527f93fe
Miguel Leon <migan95802@gmail.com>
2020-10-04 21:17:42 -0600

    Arreglo seeding base de datos

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -14,9 +17,12 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
+            'id' => 100,
             'name' => 'admin',
             'email' => 'admin@test.com',
+            'email_verified_at'=> now(),
             'password' => Hash::make('admin'),
+            'remember_token' => Str::random(10)
         ]);
 
     }
